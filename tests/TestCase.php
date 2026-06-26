a851520bae0db123a5386274f0d53c5834dbe616
amienhazami <amienhazami@gmail.com>
2026-06-26 13:45:54 +0700

    chore: silence E_DEPRECATED warnings in test suite on PHP 8.5

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,15 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        set_error_handler(function ($errno, $errstr) {
+            if ($errno === E_DEPRECATED || $errno === E_USER_DEPRECATED) {
+                return true;
+            }
+            return false;
+        }, E_DEPRECATED | E_USER_DEPRECATED);
+
+        parent::setUp();
+    }
 }
